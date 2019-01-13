d72bdc79294d337532f61e78f137811717424ab4
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-13 02:16:28 +0100

    Create function stubs

---
+++ func.go
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                      License: MIT
+// :v: 2019-01-13 02:15:07 4930D4                             go-delta/[func.go]
+// -----------------------------------------------------------------------------
+
+package bdelta
+
+type Diff []byte
+
+// ApplyDiff __
+func ApplyDiff(source []byte, diff Diff) []byte {
+	return []byte{}
+} //                                                                   ApplyDiff
+
+// MakeDiff __
+func MakeDiff(a, b []byte) Diff {
+	return Diff{}
+} //                                                                    MakeDiff
+
+//end
---
+++ func_test.go
@@ -0,0 +1,21 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                      License: MIT
+// :v: 2019-01-13 02:15:33 D0022D                        go-delta/[func_test.go]
+// -----------------------------------------------------------------------------
+
+package bdelta
+
+/*
+to test all items in dir_watcher_windows.go use:
+    go test --run Test_gdlt_
+
+to generate a test coverage report for the whole module use:
+    go test -coverprofile cover.out
+    go tool cover -html=cover.out
+*/
+
+import (
+	"testing"
+)
+
+//end

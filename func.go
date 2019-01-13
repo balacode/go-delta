109c48f9ad8342bdce1b40d53b19bfa2168f6887
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-13 18:07:12 +0100

    Create MakeMap() function

--- func.go
+++ func.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-13 02:15:07 4930D4                             go-delta/[func.go]
+// :v: 2019-01-13 18:06:53 C7CD93                             go-delta/[func.go]
 // -----------------------------------------------------------------------------
 
 package bdelta
@@ -16,5 +16,23 @@ func ApplyDiff(source []byte, diff Diff) []byte {
 func MakeDiff(a, b []byte) Diff {
 	return Diff{}
 } //                                                                    MakeDiff
+
+func MakeMap(ar []byte) (m map[[8]byte][]int) {
+	m = make(map[[8]byte][]int, 0)
+	if len(ar) < 8 {
+		return
+	}
+	var k [8]byte
+	for i := 0; i < len(ar)-8; i++ {
+		copy(k[:], ar[i:])
+		var _, exist = m[k]
+		if exist {
+			m[k] = append(m[k], i)
+		} else {
+			m[k] = []int{i}
+		}
+	}
+	return m
+} //                                                                     MakeMap
 
 //end

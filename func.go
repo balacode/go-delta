50152d226814630e7afeafb52975307f556be20c
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-13 18:13:58 +0100

    Create 'ChunkSize' constant

--- func.go
+++ func.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-13 18:06:53 C7CD93                             go-delta/[func.go]
+// :v: 2019-01-13 18:12:48 B32132                             go-delta/[func.go]
 // -----------------------------------------------------------------------------
 
 package bdelta
@@ -17,13 +17,15 @@ func MakeDiff(a, b []byte) Diff {
 	return Diff{}
 } //                                                                    MakeDiff
 
-func MakeMap(ar []byte) (m map[[8]byte][]int) {
-	m = make(map[[8]byte][]int, 0)
-	if len(ar) < 8 {
+const ChunkSize = 8
+
+func MakeMap(ar []byte) (m map[[ChunkSize]byte][]int) {
+	m = make(map[[ChunkSize]byte][]int, 0)
+	if len(ar) < ChunkSize {
 		return
 	}
-	var k [8]byte
-	for i := 0; i < len(ar)-8; i++ {
+	var k [ChunkSize]byte
+	for i := 0; i < len(ar)-ChunkSize; i++ {
 		copy(k[:], ar[i:])
 		var _, exist = m[k]
 		if exist {

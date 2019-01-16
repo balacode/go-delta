9a21fe9d81ab26a0f6000b6e3fcd6656725cc704
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-16 15:12:47 +0100

    Improve MakeDiff() unit test

--- func_test.go
+++ func_test.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-16 15:07:20 3DD72D                        go-delta/[func_test.go]
+// :v: 2019-01-16 15:11:43 33508E                        go-delta/[func_test.go]
 // -----------------------------------------------------------------------------
 
 package bdelta
@@ -60,19 +60,60 @@ func Test1(t *testing.T) {
 
 // go test --run Test_MakeDiff_
 func Test_MakeDiff_(t *testing.T) {
-	var a = []byte("ABCDEFGHIJKLM" + " " +
-		"ABCDEFGHIJKLMNOPQRSTUVWX" + " " +
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	)
-	var b = []byte("0x0x0xABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " +
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " +
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " + "0123456789",
-	)
+	var a, b []byte
+	switch 3 {
+	case 1:
+		a = []byte("ABCDEFGHIJKLM" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWX" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		)
+		b = []byte("0x0x0xABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ" + " " + "0123456789",
+		)
+	case 2:
+		a = []byte("ABCDEFGHIJKLM" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWX" + " " +
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		)
+		b = []byte("abcdefghijklmnopqrstuvwxyz" + " " +
+			"abcdefghijklmnopqrstuvwxyz" + " " +
+			"abcdefghijklmnopqrstuvwxyz" + " " + "0123456789",
+		)
+	case 3:
+		/*
+			size:           16,994,304
+			unzipped delta:  1,855,440
+			zipped delta:      704,583 (4% of file size)
+			elapsed time:   171.4 seconds
+			 171.25880: MakeDiff()
+			   0.16411: makeHash()
+			   3.78551: makeMap()
+			 165.82172: longestMatch()
+			   0.09878: appendPart()
+			   0.03101: append new
+			   0.13109: compressBytes()
+		*/
+		a = readData("test1.file")
+		b = readData("test2.file")
+		PL("loaded data")
+	case 4:
+		/*
+			target size:    10,356,821
+			unzipped delta:  5,414,754
+			zipped delta:    5,258,684 (50.7% of file size)
+			elapsed time:          6.2 seconds
+		*/
+		a = readData("test1.zip")
+		b = readData("test2.zip")
+		PL("loaded data")
+	}
 	if DebugTiming {
 		tmr.Start("MakeDiff")
 	}
 	{
-		MakeDiff(a, b)
+		var diff = MakeDiff(a, b)
+		diff.Bytes()
 	}
 	if DebugTiming {
 		tmr.Stop("MakeDiff")

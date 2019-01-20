f06514648dd2a83f60decc026d3433b9931d8cd1
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-20 07:48:51 +0100

    Move loadDelta() to delta_internal.go

--- delta.go
+++ delta.go
@@ -1,14 +1,9 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-20 07:34:51 5F174D                            go-delta/[delta.go]
+// :v: 2019-01-20 07:48:27 3CE426                            go-delta/[delta.go]
 // -----------------------------------------------------------------------------
 
 package delta
-
-import (
-	"bytes"
-	"encoding/binary"
-)
 
 // Delta stores the binary delta difference between two byte arrays
 type Delta struct {
@@ -54,75 +49,5 @@ func (ob *Delta) SourceSize() int {
 func (ob *Delta) TargetSize() int {
 	return ob.targetSize
 } //                                                                  TargetSize
-
-// -----------------------------------------------------------------------------
-// # Internal Methods
-
-// loadDelta __
-func loadDelta(data []byte) (Delta, error) {
-	//
-	// uncompress the delta
-	if DebugInfo {
-		PL("loadDelta: compressed delta length:", len(data))
-	}
-	data = uncompressBytes(data)
-	if DebugInfo {
-		PL("loadDelta: uncompressed delta length:", len(data))
-	}
-	var buf = bytes.NewBuffer(data)
-	var readInt = func() int {
-		var i int32
-		err := binary.Read(buf, binary.BigEndian, &i)
-		if err != nil {
-			mod.Error("readInt() failed:", err)
-			return -1
-		}
-		return int(i)
-	}
-	var readBytes = func() []byte {
-		var size int32
-		err := binary.Read(buf, binary.BigEndian, &size)
-		if err != nil {
-			mod.Error("readBytes() failed @1:", err)
-		}
-		var ar = make([]byte, size)
-		var nread int
-		nread, err = buf.Read(ar)
-		if err != nil {
-			mod.Error("readBytes() failed @2:", err)
-		}
-		if nread != int(size) {
-			mod.Error("readBytes() failed @3: size:", size, "nread:", nread)
-		}
-		return ar
-	}
-	// read the header
-	var ret = Delta{
-		sourceSize: readInt(),
-		sourceHash: readBytes(),
-		targetSize: readInt(),
-		targetHash: readBytes(),
-		newCount:   readInt(),
-		oldCount:   readInt(),
-	}
-	// read the parts
-	var count = readInt()
-	if count < 1 {
-		return Delta{},
-			mod.Error("readBytes() failed @4: invalid number of parts:", count)
-	}
-	ret.parts = make([]deltaPart, count)
-	for i := range ret.parts {
-		var pt = &ret.parts[i]
-		pt.sourceLoc = readInt()
-		if pt.sourceLoc == -1 {
-			pt.data = readBytes()
-			pt.size = len(pt.data)
-			continue
-		}
-		pt.size = readInt()
-	}
-	return ret, nil
-} //                                                                   loadDelta
 
 //end
--- delta_internal.go
+++ delta_internal.go
@@ -1,9 +1,81 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-20 07:40:13 180E22                   go-delta/[delta_internal.go]
+// :v: 2019-01-20 07:48:27 EBD345                   go-delta/[delta_internal.go]
 // -----------------------------------------------------------------------------
 
 package delta
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// loadDelta __
+func loadDelta(data []byte) (Delta, error) {
+	//
+	// uncompress the delta
+	if DebugInfo {
+		PL("loadDelta: compressed delta length:", len(data))
+	}
+	data = uncompressBytes(data)
+	if DebugInfo {
+		PL("loadDelta: uncompressed delta length:", len(data))
+	}
+	var buf = bytes.NewBuffer(data)
+	var readInt = func() int {
+		var i int32
+		err := binary.Read(buf, binary.BigEndian, &i)
+		if err != nil {
+			mod.Error("readInt() failed:", err)
+			return -1
+		}
+		return int(i)
+	}
+	var readBytes = func() []byte {
+		var size int32
+		err := binary.Read(buf, binary.BigEndian, &size)
+		if err != nil {
+			mod.Error("readBytes() failed @1:", err)
+		}
+		var ar = make([]byte, size)
+		var nread int
+		nread, err = buf.Read(ar)
+		if err != nil {
+			mod.Error("readBytes() failed @2:", err)
+		}
+		if nread != int(size) {
+			mod.Error("readBytes() failed @3: size:", size, "nread:", nread)
+		}
+		return ar
+	}
+	// read the header
+	var ret = Delta{
+		sourceSize: readInt(),
+		sourceHash: readBytes(),
+		targetSize: readInt(),
+		targetHash: readBytes(),
+		newCount:   readInt(),
+		oldCount:   readInt(),
+	}
+	// read the parts
+	var count = readInt()
+	if count < 1 {
+		return Delta{},
+			mod.Error("readBytes() failed @4: invalid number of parts:", count)
+	}
+	ret.parts = make([]deltaPart, count)
+	for i := range ret.parts {
+		var pt = &ret.parts[i]
+		pt.sourceLoc = readInt()
+		if pt.sourceLoc == -1 {
+			pt.data = readBytes()
+			pt.size = len(pt.data)
+			continue
+		}
+		pt.size = readInt()
+	}
+	return ret, nil
+} //                                                                   loadDelta
 
 // write appends binary difference data
 func (ob *Delta) write(sourceLoc, size int, data []byte) {

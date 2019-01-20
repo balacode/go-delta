9efa505cf33ed19c767e8500a1b1c31db3f11fae
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-20 07:28:49 +0100

    Move Delta.write() to delta_write.go

---
+++ delta_write.go
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                      License: MIT
+// :v: 2019-01-20 07:27:35 A0BDCF                      go-delta/[delta_write.go]
+// -----------------------------------------------------------------------------
+
+package delta
+
+// write appends binary difference data
+func (ob *Delta) write(sourceLoc, size int, data []byte) {
+	if DebugTiming {
+		tmr.Start("write")
+		defer tmr.Stop("write")
+	}
+	if DebugInfo && DebugWriteArgs {
+		PL("write",
+			"sourceLoc:", sourceLoc,
+			"size:", size,
+			"data:", data, string(data))
+	}
+	// argument validations
+	switch {
+	case sourceLoc < -1:
+		mod.Error("sourceLoc:", sourceLoc, " < -1")
+		return
+	case sourceLoc == -1 && len(data) == 0:
+		mod.Error("sourceLoc == -1 && len(data) == 0")
+		return
+	case sourceLoc != -1 && len(data) != 0:
+		mod.Error("sourceLoc != -1 && len(data):", len(data), "!= 0")
+		return
+	case size < 1:
+		mod.Error("size:", size, " < 1")
+		return
+	}
+	// if the previous part was embedded directly, append to that part's data
+	if sourceLoc == -1 {
+		var n = len(ob.parts)
+		if n > 0 {
+			var last = &ob.parts[n-1]
+			if last.sourceLoc == -1 {
+				last.size += len(data)
+				last.data = append(last.data, data...)
+				return
+			}
+		}
+	}
+	// append a new part
+	var ar []byte
+	if sourceLoc == -1 {
+		ar = make([]byte, len(data))
+		copy(ar, data)
+	}
+	ob.parts = append(ob.parts,
+		deltaPart{sourceLoc: sourceLoc, size: size, data: ar})
+} //                                                                       write
+
+//end

af7d0b935ee53453acf5686d2c66b154a33e772f
balarabe@protonmail.com <balarabe@protonmail.com>
2019-01-22 11:09:44 +0100

    Format

--- module.go
+++ module.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2019-01-20 07:25:22 4A054E                           go-delta/[module.go]
+// :v: 2019-01-22 11:09:26 0E2FC3                           go-delta/[module.go]
 // -----------------------------------------------------------------------------
 
 package delta
@@ -20,6 +20,15 @@ const MatchLimit = 50
 // MatchSize specifies the size of unique chunks being searched for, in bytes.
 const MatchSize = 8
 
+// PL is fmt.Println() but is used only for debugging.
+var PL = fmt.Println
+
+// tmr is used for timing all methods/functions during tuning.
+var tmr zr.Timer
+
+// -----------------------------------------------------------------------------
+// # Debugging Flags
+
 // DebugInfo when set, causes printing of messages helpful for debugging.
 var DebugInfo = false
 
@@ -28,12 +37,6 @@ var DebugTiming = true
 
 // DebugWriteArgs when set, prints the arguments passed to write()
 var DebugWriteArgs = false
-
-// PL is fmt.Println() but is used only for debugging.
-var PL = fmt.Println
-
-// tmr is used for timing all methods/functions during tuning.
-var tmr zr.Timer
 
 // -----------------------------------------------------------------------------
 // # Function Proxy Variables (for mocking)
